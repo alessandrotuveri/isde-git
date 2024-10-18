fd0a4a0693b0fe2596866030f0c46f0d6e4b9f7a
Giulia <giulia.melis0401@gmail.com>
2024-10-18 17:44:12 +0200

    predict function implementation

--- src/classifiers/nmc.py
+++ src/classifiers/nmc.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics import pairwise_distances
 
 
 class NMC(object):
@@ -22,20 +22,50 @@ class NMC(object):
 
     """
 
-    def __init__(self):
-        self._centroids = None
-        self._class_labels = None  # class labels may not be contiguous indices
+    class NMC:
+        """
+        Class defined for NMC classifier.
+        """
 
-    @property
-    def centroids(self):
-        return self._centroids
+        def __init__(self):
+            self._centroids = None
 
-    @property
-    def class_labels(self):
-        return self._class_labels
+        @property
+        def centroids(self):
+            return self._centroids
 
-    def fit(self, xtr, ytr):
-        pass
+        # @centroids.setter
+        # def centroids(self, value):
+        #    self._centroids = value
 
-    def predict(self, xts):
-        pass
+        def fit(self, xtr, ytr):
+            """
+            Compute the average centroids for each class
+
+            Parameters
+            ----------
+            xtr: training data
+            ytr: training labels
+
+            Returns
+            -------
+            self: trained NMC classifier
+            """
+
+            pass
+
+        def predict(self, xts):
+            """
+            Brand new docstring
+
+            Parameters
+            ----------
+            xts
+
+            Returns
+            -------
+
+            """
+            scores = self.decision_function(xts)
+            ypred = np.argmax(scores, axis=1)
+            return ypred
